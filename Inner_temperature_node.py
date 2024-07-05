55104d7a20d6813220529d225255cb55ec937406
AndreaCalabro-AYES <andrea.calabro@ayesconsulting.com>
2024-07-05 18:23:08 +0200

    messages shall be sent in a dictionary in a json file, new method will arrive

--- Inner_temperature_node.py
+++ Inner_temperature_node.py
@@ -1,6 +1,7 @@
 from mqtt_client_ayes import AyesMqttClient
 import os
 import time
+import json
 
 MQTT_BROKER_HOST = os.getenv(key="MQTT_BROKER_HOST", default="mqtt_broker")
 MQTT_BROKER_PORT = int(os.getenv(key="MQTT_BROKER_PORT", default=1883))
@@ -10,7 +11,7 @@ inner_temp_list = [17.2, 18.9, 12.4, 14.4, 16.4, 17.1, 18.0, 22.1, 33.9]
 inner_temp_client = AyesMqttClient(
     broker= MQTT_BROKER_HOST,
     port= MQTT_BROKER_PORT,
-    topics_list= ["temperature/internal", "greetings/face_removed"],
+    topics_list= ["temperature/internal", "greetings/face_removed", "greetings/face_added"],
     client_id= "internal_temperature"
 )
 
@@ -19,10 +20,13 @@ inner_temp_client.connect()
 
 
 for temp in inner_temp_list:
-    time.sleep(3)
+    
+    msg_body = json.dumps({"temperature": temp})
+    
+    time.sleep(10)
+    
     inner_temp_client.publish_message(
         topic= "temperature/internal",
-        message= temp
+        payload= msg_body
     )
     
-    print(inner_temp_client.on_message['topic'])
